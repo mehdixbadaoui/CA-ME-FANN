72cc471d6ee9e9b52faa9337bcd569b2e66730c9
Mehdi <mehdixbadaoui@outlook.fr>
2020-10-14 11:55:59 +0200

    project creation

---
+++ FANN/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FANNCSharp;
+using FANN;
+
+namespace FANN
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("hello");
+            //Console.ReadLine();
+        }
+    }
+}
