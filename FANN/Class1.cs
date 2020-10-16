e5760edcac561fa182e1f965a1b728e473cf019c
mehdi <mehdixbadaoui@outlook.fr>
2020-10-16 17:42:59 +0200

    just counting frequencies

--- FANN/Class1.cs
+++ FANN/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -11,10 +12,35 @@ namespace FANN
 {
     class Program
     {
+        private static int charindex(char c)
+        {
+            return (int)c % 32;
+        }
+
         public static void Main()
         {
-            Console.WriteLine("hello");
-            //Console.ReadLine();
+            float[] letter_frequ = new float[27];
+
+            string file = "C:\\Users\\mebadaoui\\Documents\\AI\\CA-ME-FANN\\FANN\\text.txt";
+            if (File.Exists(file))
+            {
+                int num_chars = 0;
+                string body = File.ReadAllText(file);
+                foreach (char c in body)
+                {
+                    if(c != ' ') num_chars++;
+                }
+
+                foreach (char c in body)
+                {
+                    //Console.Write(charindex(c) + " ");
+                    letter_frequ[charindex(c)] = (letter_frequ[charindex(c)] + 1) / num_chars;
+                }
+
+                Console.WriteLine(letter_frequ[1]);
+            }
+
+            Console.Read();
         }
     }
 }
