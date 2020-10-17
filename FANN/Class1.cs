8f1fbdc72ceafd82ceea0d1b91f1e13b87206b20
Mehdi <mehdixbadaoui@outlook.fr>
2020-10-17 21:56:48 +0200

    add data to training file

--- FANN/Class1.cs
+++ FANN/Class1.cs
@@ -17,28 +17,75 @@ namespace FANN
             return (int)c % 32;
         }
 
+        static double[] calc_frequencies(string file)
+        {
+            string text = File.ReadAllText(file);
+            double[] letter_frequ = new double[27];
+
+            int num_chars = 0;
+            foreach (char c in text)
+            {
+                if (char.ToLower(c) >= 97 && char.ToLower(c) < 123) num_chars++;
+            }
+            Console.WriteLine(num_chars);
+            foreach (char c in text)
+            {
+                Console.Write(charindex(char.ToLower(c)) + " ");
+                letter_frequ[charindex(char.ToLower(c))] += (1f/num_chars);
+            }
+            //Console.WriteLine("\n" + (double)1/5);
+
+            foreach(double f in letter_frequ)
+            {
+                Console.Write(f+ " ");
+            }
+            Console.WriteLine();
+
+
+            return letter_frequ;
+
+        }
+
+        static void fill_train_file(string file_in, string file_out)
+        {
+            double[] frequencies = calc_frequencies(file_in);
+
+            if (File.Exists(file_out))
+            {
+                foreach (double freq in frequencies)
+                {
+                    Console.Write(freq.ToString() + " ");
+                }
+
+
+                using (StreamWriter sw = File.AppendText(file_out))
+                {
+
+                    sw.Write("\n1 0 0\n");
+                    foreach (float freq in frequencies)
+                    {
+                        sw.Write(freq.ToString() + " ");
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("no file");
+            }
+
+        }
         public static void Main()
         {
-            float[] letter_frequ = new float[27];
 
-            string file = "C:\\Users\\mebadaoui\\Documents\\AI\\CA-ME-FANN\\FANN\\text.txt";
-            if (File.Exists(file))
-            {
-                int num_chars = 0;
-                string body = File.ReadAllText(file);
-                foreach (char c in body)
-                {
-                    if(c != ' ') num_chars++;
-                }
+            string file_in = Directory.GetCurrentDirectory() + "\\text.txt";
+            string file_out = Directory.GetCurrentDirectory() + "\\training.txt";
+            //Console.WriteLine((int)'z');
 
-                foreach (char c in body)
-                {
-                    //Console.Write(charindex(c) + " ");
-                    letter_frequ[charindex(c)] = (letter_frequ[charindex(c)] + 1) / num_chars;
-                }
-
-                Console.WriteLine(letter_frequ[1]);
-            }
+            fill_train_file(file_in, file_out);
+            //if (File.Exists(file))
+            //string body = File.ReadAllText(file);
 
             Console.Read();
         }
